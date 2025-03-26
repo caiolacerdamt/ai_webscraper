161397911a9c47b1f82b7cd3622d59e4b4b4611e
caiolacerdamt <caiolacerdamt@gmail.com>
2025-03-26 17:50:02 +0300

    simple scraping structure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from scrape import scrape_web
 
 st.title("AI Webscraper")
 
@@ -6,3 +7,5 @@ url = st.text_input("Website URL: ")
 
 if st.button("Scrape Site"):
     st.write("Scraping the website..")
+    result = scrape_web(url)
+    print(result)
